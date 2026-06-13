cf74a395a05992f40e1b5f617e8926a58827376c
CleyJosRM <cleyjoserm2@gmail.com>
2026-06-13 06:20:18 -0300

    Apagando documentação interna de src/arvoreb/arvoreb_io.c. Toda documentação de funções necessárias presentes no seu respectivo header

--- src/arvoreb/arvoreb_io.c
+++ src/arvoreb/arvoreb_io.c
@@ -1,52 +1,38 @@
+/**
+ * ============================================================================
+ * @file        arvoreb_io.c
+ * @brief       Implementação de funções de leitura e escrita em arquivos para a Árvore-B, incluindo manipulação de nós e cabeçalho.
+ *
+ * @author      Cleyton Jose Rodrigues Macedo
+ * @author      Guilherme Cavalcanti de Santana
+ *
+ * @details
+ * 
+ * Em todas as funções, é fornecida uma filestream para acessar o arquivo
+ *   e o endereço de um buffer na memória onde os bytes serão lidos/escritos.
+ * As funções do tipo "carregar" lêem bytes do disco e os escrevem na memória principal.
+ * As funções do tipo "armazenar" lêem bytes da memória principal e os escrevem no disco.
+ * As permissões da filestream devem ser adequadas e o buffer deve conter espaço suficiente.
+ * ============================================================================
+ */
+
 #include "../../include/arvoreb/arvoreb_interna.h"
 
-/*
-FUNÇÕES QUE ESCREVEM E LÊEM DO DISCO
-Em todas as funções, é fornecida uma filestream para acessar o arquivo
-e o endereço de um buffer na memória onde os bytes serão lidos/escritos.
-As funções do tipo "carregar" lêem bytes do disco e os escrevem na memória principal.
-As funções do tipo "armazenar" lêem bytes da memória principal e os escrevem no disco.
-As permissões da filestream devem ser adequadas e o buffer deve conter espaço suficiente.
-*/
-
-
-
-
-
-/**
- * Transfere um B-nó da memória principal para o arquivo. 
- * A posição onde se deseja inserir esse B-nó deve ser especificada como RRN.
- * A filestream deve permitir escrita.
- */
 void armazenar_no(FILE* arvoreB, const byteBTree* no, int RRN){
     fseek(arvoreB, TAM_CABECALHO_BTREE+TAM_NO_BTREE*RRN, SEEK_SET); // calculando byteoffset e posicionando o cursor
     fwrite(no, TAM_NO_BTREE, 1, arvoreB); // escrevendo o conteúdo da memória no disco
 }
 
-/**
- * Transfere um B-nó do arquivo para a memória principal. 
- * Para achar esse B-nó, deve ser fornecido o RRN dele.
- * A filestream deve permitir leitura.
- */
 void carregar_no(byteBTree* buffer, FILE* arvoreB, int RRN){
     fseek(arvoreB, TAM_CABECALHO_BTREE+TAM_NO_BTREE*RRN, SEEK_SET); // calculando byteoffset e posicionando o cursor
     fread(buffer, TAM_NO_BTREE, 1, arvoreB); // lendo o conteúdo do disco para a memória
 }
 
-/**
- * Transfere o cabeçalho da árvore-B da memória principal para o arquivo.
- * A filestream deve permitir escrita.
- */
 void armazenar_cabecalho(FILE* arvoreB, const byteBTree* buffer){
     fseek(arvoreB, 0, SEEK_SET); // calculando byteoffset e posicionando o cursor
     fwrite(buffer, TAM_CABECALHO_BTREE, 1, arvoreB); // escrevendo o conteúdo da memória no disco
 }
 
-/**
- * Transfere o cabeçalho da árvore-B do arquivo para a memória principal. 
- * Caso statusInconsistente seja true, o status do arquivo é marcado como '0' antes de retornar.
- * A filestream deve permitir leitura e escrita, mas a escrita só acontece quando statusInconsistente é true.
- */
 void carregar_cabecalho(byteBTree* buffer, FILE* arvoreB, bool statusInconsistente){
     fseek(arvoreB, 0, SEEK_SET); // calculando byteoffset e posicionando o cursor
     fread(buffer, TAM_CABECALHO_BTREE, 1, arvoreB); // lendo o conteúdo do disco para a memória
